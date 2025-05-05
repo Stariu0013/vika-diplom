0839dc95895f834f671b2d436b86f7874092f98f
Stariu <v.starynets0013@gmail.com>
2025-05-05 16:04:39 +0100

    update folder

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const authRoutes = require('../routes/auth');
-const stockRoutes = require('../routes/stocks');
-const portfolioRoutes = require('../routes/portfolio');
-const activeRoutes = require('../routes/active');
-const {AUTH_ROUTE, STOCK_ROUTE, PORTFOLIOS_ROUTE, ACTIVES_ROUTE} = require("../consts");
+const authRoutes = require('./routes/auth');
+const stockRoutes = require('./routes/stocks');
+const portfolioRoutes = require('./routes/portfolio');
+const activeRoutes = require('./routes/active');
+const {AUTH_ROUTE, STOCK_ROUTE, PORTFOLIOS_ROUTE, ACTIVES_ROUTE} = require("./consts");
 const { MONGODB_URL, PORT = 5000, SESSION_SECRET_KEY } = process.env;
 
 function initializeMiddleware(app) {
