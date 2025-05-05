0f9e56f21d5cd5252069465604bb4d1cbe19d6b2
Stariu <v.starynets0013@gmail.com>
2025-05-05 15:54:08 +0100

    update vercel

--- server/api/server.js
+++ server/api/server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const authRoutes = require('./routes/auth');
-const stockRoutes = require('./routes/stocks');
-const portfolioRoutes = require('./routes/portfolio');
-const activeRoutes = require('./routes/active');
-const {AUTH_ROUTE, STOCK_ROUTE, PORTFOLIOS_ROUTE, ACTIVES_ROUTE} = require("./consts");
+const authRoutes = require('../routes/auth');
+const stockRoutes = require('../routes/stocks');
+const portfolioRoutes = require('../routes/portfolio');
+const activeRoutes = require('../routes/active');
+const {AUTH_ROUTE, STOCK_ROUTE, PORTFOLIOS_ROUTE, ACTIVES_ROUTE} = require("../consts");
 const { MONGODB_URL, PORT = 5000, SESSION_SECRET_KEY } = process.env;
 
 function initializeMiddleware(app) {
@@ -37,6 +37,10 @@ app.use(AUTH_ROUTE, authRoutes);
 app.use(STOCK_ROUTE, stockRoutes);
 app.use(PORTFOLIOS_ROUTE, portfolioRoutes);
 app.use(ACTIVES_ROUTE, activeRoutes);
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ message: 'Hello World!'});
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
